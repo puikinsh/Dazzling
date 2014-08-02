b59028aec045d50ca67944aebe9b157179cbbb85
sportpal <admin@sportpal.org>
2014-08-02 04:34:23 -0400

    Update functions.php
    removed skip-link-focus-fix.js and functions.min.js reference and created merged them into the main.js where all custom JS to be added

--- functions.php
+++ functions.php
@@ -168,9 +168,7 @@ function dazzling_scripts() {
 		wp_enqueue_script( 'flexslider', get_template_directory_uri() . '/inc/js/flexslider.min.js', array('jquery'), '20140222', true );
 	}
 
-	wp_enqueue_script( 'dazzling-bootstrapwp', get_template_directory_uri() . '/inc/js/functions.min.js', array('jquery') );
-
-	wp_enqueue_script( 'dazzling-skip-link-focus-fix', get_template_directory_uri() . '/inc/js/skip-link-focus-fix.js', array(), '20140222', true );
+	wp_enqueue_script( 'dazzling-main', get_template_directory_uri() . '/inc/js/main.js', array('jquery') );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
