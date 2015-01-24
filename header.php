3cc7a9ab061ac6a5b8a2c037faea353436b43caf
puikinsh <aigars.silkalns@gmail.com>
2015-01-24 13:17:20 +0200

    Tiny code styling improvements

--- header.php
+++ header.php
@@ -33,13 +33,13 @@
 
 	<nav class="navbar navbar-default" role="navigation">
 		<div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			    <span class="sr-only">Toggle navigation</span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			  </button>
 
 				<?php if( get_header_image() != '' ) : ?>
 
@@ -57,7 +57,7 @@
 
 				<?php endif; // header image was removed (again) ?>
 
-	    </div>
+			</div>
 				<?php dazzling_header_menu(); ?>
-	  </div>
+		</div>
 	</nav><!-- .site-navigation -->
