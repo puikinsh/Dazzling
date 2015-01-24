c66ed4e209e0ab0b44432e91b792e627c538e8a9
puikinsh <aigars.silkalns@gmail.com>
2015-01-24 13:11:22 +0200

    Tiny code styling

--- functions.php
+++ functions.php
@@ -76,14 +76,14 @@ add_action( 'after_setup_theme', 'dazzling_setup' );
  * Register widgetized area and update sidebar with default widgets.
  */
 function dazzling_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'dazzling' ),
-		'id'            => 'sidebar-1',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h3 class="widget-title">',
-		'after_title'   => '</h3>',
-	) );
+  register_sidebar( array(
+    'name'          => __( 'Sidebar', 'dazzling' ),
+    'id'            => 'sidebar-1',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
+  ) );
   register_sidebar(array(
     'id'            => 'home-widget-1',
     'name'          => __( 'Homepage Widget 1', 'dazzling' ),
