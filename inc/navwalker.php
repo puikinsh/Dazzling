0a2014a28d3015aa386db2309670da3d7f917fd9
Free WP TP <freewptp@gmail.com>
2015-12-24 19:46:27 +0530

    Made menu item display Title Attrivute as title instead of Navigation Label. Issue - #30

--- inc/navwalker.php
+++ inc/navwalker.php
@@ -76,9 +76,9 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
                         $output .= $indent . '<li' . $id . $value . $class_names .'>';
 
                         $atts = array();
-                        $atts['title']  = ! empty( $item->title )        ? $item->title        : '';
-                        $atts['target'] = ! empty( $item->target )        ? $item->target        : '';
-                        $atts['rel']    = ! empty( $item->xfn )                ? $item->xfn        : '';
+                        $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : $item->title;
+                        $atts['target'] = ! empty( $item->target ) ? $item->target : '';
+                        $atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
 
                         // If item has_children add atts to a.
                         if ( $args->has_children && $depth === 0 ) {
